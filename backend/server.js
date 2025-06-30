1dc8566c77e4012b3e0afc5a2b7c2092124503fb
root <root@edge-of-sanity.fritz.box>
2025-06-30 09:51:57 +0000

    Fix: Docker Compose volume configuration and implement data persistence

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,76 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+const mountsFilePath = path.join(__dirname, 'mounts.json');
+
+let mounts = [];
+
+// Load mounts from file on startup
+function loadMounts() {
+    if (fs.existsSync(mountsFilePath)) {
+        const data = fs.readFileSync(mountsFilePath, 'utf8');
+        mounts = JSON.parse(data);
+    } else {
+        fs.writeFileSync(mountsFilePath, JSON.stringify([]), 'utf8');
+    }
+}
+
+// Save mounts to file
+function saveMounts() {
+    fs.writeFileSync(mountsFilePath, JSON.stringify(mounts, null, 2), 'utf8');
+}
+
+loadMounts();
+
 app.use(cors());
 app.use(express.json());
+
+// API to get all mounts
+app.get('/api/mounts', (req, res) => {
+    res.json(mounts);
+});
+
+// API to add a new mount
+app.post('/api/mounts', (req, res) => {
+    const newMount = { id: Date.now(), ...req.body };
+    mounts.push(newMount);
+    saveMounts();
+    res.status(201).json(newMount);
+});
+
+// API to update a mount
+app.put('/api/mounts/:id', (req, res) => {
+    const { id } = req.params;
+    const mountIndex = mounts.findIndex(m => m.id == id);
+
+    if (mountIndex > -1) {
+        mounts[mountIndex] = { ...mounts[mountIndex], ...req.body };
+        saveMounts();
+        res.json(mounts[mountIndex]);
+    } else {
+        res.status(404).json({ message: 'Mount not found' });
+    }
+});
+
+// API to delete a mount
+app.delete('/api/mounts/:id', (req, res) => {
+    const { id } = req.params;
+    const initialLength = mounts.length;
+    mounts = mounts.filter(m => m.id != id);
+
+    if (mounts.length < initialLength) {
+        saveMounts();
+        res.status(204).send(); // No Content
+    } else {
+        res.status(404).json({ message: 'Mount not found' });
+    }
+});
 
 app.post('/api/mount-image', async (req, res) => {
     const { mountName } = req.body;
